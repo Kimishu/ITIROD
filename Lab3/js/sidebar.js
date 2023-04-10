6f2e1600c85d0cbdab8dd6fb3d6be9324b19fc09
Kimihito <lambent.darkness@mail.ru>
2023-04-11 02:42:44 +0300

    Another fixes.. JESUS

--- Lab3/js/sidebar.js
+++ Lab3/js/sidebar.js
@@ -1,14 +1,4 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-function setNav(){
+function getNav(){
     if(document.getElementById("mySidenav").style.width == "0px"){
         document.getElementById("mySidenav").style.width = "250px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
